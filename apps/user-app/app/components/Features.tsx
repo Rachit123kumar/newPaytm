aeaa25f4c78c913a7ce4967b69377738e239209f
Rachit123kumar <hellobittukumar12@gmail.com>
2024-09-13 07:48:38 +0530

    updating services in features section

---
+++ apps/user-app/app/components/Features.tsx
@@ -0,0 +1,470 @@
+
+
+export default function Features() {
+  return (
+    <div className="bg-[#F0FAFD] p-8 ">
+    <div>
+      <p className="text-center text-2xl font-semibold mb-24">
+        A universal platform for universal growth{" "}
+      </p>
+    </div>
+    <div className="grid lg:grid-rows-2 grid-flow-col gap-8 sm:grid-rows-3 ">
+      <div className="w-max-[300px] bg-green-100 p-8 sm:p-4 rounded-sm">
+        <div className="mb-4 sm:mb-2">
+          <div className="flex gap-2 items-center">
+            <svg
+              width="4"
+              height="50"
+              viewBox="0 0 4 53"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="ml-[-5px]"
+            >
+              <path
+                d="M2 0L2 53"
+                stroke="url(#paint0_linear_6_108)"
+                strokeWidth="3"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_6_108"
+                  x1="1.5"
+                  y1="0"
+                  x2="1.5"
+                  y2="53"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#F84912" />
+                  <stop offset="0.494666" stopColor="#CF3D0F" />
+                  <stop offset="1" stopColor="#1E63E9" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3"
+              />
+            </svg>
+          </div>
+        </div>
+        <p className="mb-2 sm:mb-1">Get Paid to Local Receiving accounts</p>
+        <p className="text-sm ">
+          Receive bank account numbers in EUR, USD, GBP & more and get paid
+          as easily as having a local bank account
+        </p>
+        <div className="flex justify-items-center items-center  flex-row mt-3">
+          <div className="">
+            <button className="bg-black text-white px-3 py-2 rounded-l-2xl sm:px-1  sm:text-sm">
+              Learn more{" "}
+            </button>
+          </div>
+          <div className="bg-black text-white pr-1 py-2 rounded-r-2xl ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 sm:size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="w-max-[300px] bg-green-100 p-8 sm:p-4 rounded-sm">
+        <div className="mb-4 sm:mb-2">
+          <div className="flex gap-2 items-center">
+            <svg
+              width="4"
+              height="50"
+              viewBox="0 0 4 53"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="ml-[-5px]"
+            >
+              <path
+                d="M2 0L2 53"
+                stroke="url(#paint0_linear_6_108)"
+                strokeWidth="3"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_6_108"
+                  x1="1.5"
+                  y1="0"
+                  x2="1.5"
+                  y2="53"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#F84912" />
+                  <stop offset="0.494666" stopColor="#CF3D0F" />
+                  <stop offset="1" stopColor="#1E63E9" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8 hii"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <p className="mb-2 sm:mb-1">Expand into new market places</p>
+        <p className="text-sm ">
+          connect with thousand of market places ang starting get paid with
+          couples of clicks
+        </p>
+        <div className="flex justify-items-center items-center  flex-row mt-3">
+          <div className="">
+            <button className="bg-black text-white px-3 py-2 rounded-l-2xl sm:px-1  sm:text-sm">
+              Learn more{" "}
+            </button>
+          </div>
+          <div className="bg-black text-white pr-1 py-2 rounded-r-2xl ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 sm:size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="w-max-[300px] bg-green-100 p-8 sm:p-4 rounded-sm">
+        <div className="mb-4 sm:mb-2">
+          <div className="flex gap-2 items-center">
+            <svg
+              width="4"
+              height="50"
+              viewBox="0 0 4 53"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="ml-[-5px]"
+            >
+              <path
+                d="M2 0L2 53"
+                stroke="url(#paint0_linear_6_108)"
+                strokeWidth="3"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_6_108"
+                  x1="1.5"
+                  y1="0"
+                  x2="1.5"
+                  y2="53"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#F84912" />
+                  <stop offset="0.494666" stopColor="#CF3D0F" />
+                  <stop offset="1" stopColor="#1E63E9" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8 hii"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <p className="mb-2 sm:mb-1">Request a payment</p>
+        <p className="text-sm ">
+          offer your international clients a simple way to pay you with our
+          billing service
+        </p>
+        <div className="flex justify-items-center items-center  flex-row mt-3">
+          <div className="">
+            <button className="bg-black text-white px-3 py-2 rounded-l-2xl sm:px-1  sm:text-sm">
+              Learn more{" "}
+            </button>
+          </div>
+          <div className="bg-black text-white pr-1 py-2 rounded-r-2xl ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 sm:size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="w-max-[300px] bg-green-100 p-8 sm:p-4 rounded-sm">
+        <div className="mb-4 sm:mb-2">
+          <div className="flex gap-2 items-center">
+            <svg
+              width="4"
+              height="50"
+              viewBox="0 0 4 53"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="ml-[-5px]"
+            >
+              <path
+                d="M2 0L2 53"
+                stroke="url(#paint0_linear_6_108)"
+                strokeWidth="3"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_6_108"
+                  x1="1.5"
+                  y1="0"
+                  x2="1.5"
+                  y2="53"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#F84912" />
+                  <stop offset="0.494666" stopColor="#CF3D0F" />
+                  <stop offset="1" stopColor="#1E63E9" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <p className="mb-2 sm:mb-1">Pay your Vat</p>
+        <p className="text-sm ">
+          pay the vat authorities in the EU and UK with the GBP and EUR
+          funds in your Allpay account for zero fees
+        </p>
+        <div className="flex justify-items-center items-center  flex-row mt-3">
+          <div className="">
+            <button className="bg-black text-white px-3 py-2 rounded-l-2xl sm:px-1  sm:text-sm">
+              Learn more{" "}
+            </button>
+          </div>
+          <div className="bg-black text-white pr-1 py-2 rounded-r-2xl ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 sm:size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="w-max-[300px] bg-green-100 p-8 sm:p-4 rounded-sm">
+        <div className="mb-4 sm:mb-2">
+          <div className="flex gap-2 items-center">
+            <svg
+              width="4"
+              height="50"
+              viewBox="0 0 4 53"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="ml-[-5px]"
+            >
+              <path
+                d="M2 0L2 53"
+                stroke="url(#paint0_linear_6_108)"
+                strokeWidth="3"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_6_108"
+                  x1="1.5"
+                  y1="0"
+                  x2="1.5"
+                  y2="53"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#F84912" />
+                  <stop offset="0.494666" stopColor="#CF3D0F" />
+                  <stop offset="1" stopColor="#1E63E9" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8 hii"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <p className="mb-2 sm:mb-1">Get Paid to Local Receiving accounts</p>
+        <p className="text-sm ">
+          Receive bank account numbers in EUR, USD, GBP & more and get paid
+          as easily as having a local bank account
+        </p>
+        <div className="flex justify-items-center items-center  flex-row mt-3">
+          <div className="">
+            <button className="bg-black text-white px-3 py-2 rounded-l-2xl sm:px-1  sm:text-sm">
+              Learn more{" "}
+            </button>
+          </div>
+          <div className="bg-black text-white pr-1 py-2 rounded-r-2xl ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 sm:size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="w-max-[300px] bg-green-100 p-8 sm:p-4 rounded-sm">
+        <div className="mb-4 sm:mb-2">
+          <div className="flex gap-2 items-center">
+            <svg
+              width="4"
+              height="50"
+              viewBox="0 0 4 53"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="ml-[-5px]"
+            >
+              <path
+                d="M2 0L2 53"
+                stroke="url(#paint0_linear_6_108)"
+                strokeWidth="3"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_6_108"
+                  x1="1.5"
+                  y1="0"
+                  x2="1.5"
+                  y2="53"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#F84912" />
+                  <stop offset="0.494666" stopColor="#CF3D0F" />
+                  <stop offset="1" stopColor="#1E63E9" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <p className="mb-2 sm:mb-1">Receive FIRA/FIRS/NOC</p>
+        <p className="text-sm ">
+          Get automated FIRA/FIRS/NOC direct to your account for free
+        </p>
+        <div className="flex justify-items-center items-center  flex-row mt-3">
+          <div className="">
+            <button className="bg-black text-white px-3 py-2 rounded-l-2xl sm:px-1  sm:text-sm">
+              Learn more{" "}
+            </button>
+          </div>
+          <div className="bg-black text-white pr-1 py-2 rounded-r-2xl ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 sm:size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,12 @@
-
 import React from "react";
 import Mainpage from "./components/Mainpage";
-
+import Features from "./components/Features";
 
 export default function Home() {
   return (
-   <div>
-    <Mainpage/>
-   </div>
+    <div>
+      <Mainpage />
+     <Features/>
+    </div>
   );
 }
